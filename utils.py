adc44f91fc91371f94d3e1055da1edf8470d86e1
TeamDeLollis <54811620+TeamDeLollis@users.noreply.github.com>
2019-09-25 16:50:31 +0200

    Update utils.py

--- utils.py
+++ utils.py
@@ -44,20 +44,20 @@ class ImagePool(object):
 
 def load_test_data(image_path, fine_size=256):
     img = imread(image_path)
-    img = scipy.misc.imresize(img, [fine_size, fine_size])
-    img = img/127.5 - 1
+    #img = scipy.misc.imresize(img, [fine_size, fine_size])
+    img = img/255
     return img
 
 def load_train_data(image_path, load_size=286, fine_size=256, is_testing=False):
     img_A = imread(image_path[0])
     img_B = imread(image_path[1])
     if not is_testing:
-        img_A = scipy.misc.imresize(img_A, [load_size, load_size])
-        img_B = scipy.misc.imresize(img_B, [load_size, load_size])
-        h1 = int(np.ceil(np.random.uniform(1e-2, load_size-fine_size)))
-        w1 = int(np.ceil(np.random.uniform(1e-2, load_size-fine_size)))
-        img_A = img_A[h1:h1+fine_size, w1:w1+fine_size]
-        img_B = img_B[h1:h1+fine_size, w1:w1+fine_size]
+        #img_A = scipy.misc.imresize(img_A, [load_size, load_size])
+        #img_B = scipy.misc.imresize(img_B, [load_size, load_size])
+        #h1 = int(np.ceil(np.random.uniform(1e-2, load_size-fine_size)))
+        #w1 = int(np.ceil(np.random.uniform(1e-2, load_size-fine_size)))
+        #img_A = img_A[h1:h1+fine_size, w1:w1+fine_size]
+       # img_B = img_B[h1:h1+fine_size, w1:w1+fine_size]
 
         if np.random.random() > 0.5:
             img_A = np.fliplr(img_A)
@@ -66,8 +66,8 @@ def load_train_data(image_path, load_size=286, fine_size=256, is_testing=False):
         img_A = scipy.misc.imresize(img_A, [fine_size, fine_size])
         img_B = scipy.misc.imresize(img_B, [fine_size, fine_size])
 
-    img_A = img_A/127.5 - 1.
-    img_B = img_B/127.5 - 1.
+    img_A = img_A/255
+    img_B = img_B/255
 
     img_AB = np.concatenate((img_A, img_B), axis=2)
     # img_AB shape: (fine_size, fine_size, input_c_dim + output_c_dim)
